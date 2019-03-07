f696eee01c87b6d5d11466e43aa248e84c23f0e4
test <test>
2019-03-06 19:11:49 -0500

    Updates

---
+++ precache-manifest.0039f4912145b053004d33709a252aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f80a46f991ce10a0145",
+    "url": "/face-recognition/static/js/runtime~main.4f80a46f.js"
+  },
+  {
+    "revision": "76d250cbcab8a85eafff",
+    "url": "/face-recognition/static/js/main.76d250cb.chunk.js"
+  },
+  {
+    "revision": "b325ffd041754b00939d",
+    "url": "/face-recognition/static/js/2.b325ffd0.chunk.js"
+  },
+  {
+    "revision": "76d250cbcab8a85eafff",
+    "url": "/face-recognition/static/css/main.f226dbc2.chunk.css"
+  },
+  {
+    "revision": "b325ffd041754b00939d",
+    "url": "/face-recognition/static/css/2.0e8eb2a5.chunk.css"
+  },
+  {
+    "revision": "5a13313ad243e3b562c6183b125d4d32",
+    "url": "/face-recognition/index.html"
+  }
+];
